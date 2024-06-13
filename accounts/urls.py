2bb0ceeb6b8a9312a54f757dde8912eea3ad9615
Bhavya2104 <97469185+Bhavya2104@users.noreply.github.com>
2024-06-14 00:06:11 +0530

    Customer and Vendor Registration, and Dashboard functionality and login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,25 +1,25 @@
-"""
-URL configuration for FoodX_main project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
+    # path('', views.myAccount),
     path('registerUser/', views.registerUser, name='registerUser'),
+    path('registerVendor/', views.registerVendor, name='registerVendor'),
+
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('myAccount/', views.myAccount, name='myAccount'),
+    path('custDashboard/', views.custDashboard, name='custDashboard'),
+    path('vendorDashboard/', views.vendorDashboard, name='vendorDashboard'),
+
+    # path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+
+    # path('forgot_password/', views.forgot_password, name='forgot_password'),
+    # path('reset_password_validate/<uidb64>/<token>/', views.reset_password_validate, name='reset_password_validate'),
+    # path('reset_password/', views.reset_password, name='reset_password'),
+
+    # path('vendor/', include('vendor.urls')),
+    # path('customer/', include('customers.urls')),
+
 ]
---
+++ vendor/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Vendor
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'vendor_name', 'is_approved', 'created_at')
+    list_display_links = ('user', 'vendor_name')
+    list_editable = ('is_approved',)
+
+
+
+
+admin.site.register(Vendor,VendorAdmin)
