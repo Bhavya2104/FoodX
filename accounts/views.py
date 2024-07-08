58baf8be5be854af3e3eecee686351563cd54c39
Bhavya2104 <97469185+Bhavya2104@users.noreply.github.com>
2024-07-08 11:31:24 +0530

    Vendor profile done except Google address autocomplete

--- accounts/views.py
+++ accounts/views.py
@@ -96,11 +96,10 @@ def registerVendor(request):
             vendor = v_form.save(commit=False)
             vendor.user = user
             vendor_name = v_form.cleaned_data['vendor_name']
-            vendor.vendor_slug = slugify(vendor_name)+'-'+str(user.id)
+            # vendor.vendor_slug = slugify(vendor_name)+'-'+str(user.id)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
-
             # Send verification email
             mail_subject = 'Please activate your account'
             email_template = 'accounts/emails/account_verification_email.html'
@@ -190,27 +189,28 @@ def custDashboard(request):
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
     vendor = Vendor.objects.get(user=request.user)
-    orders = Order.objects.filter(vendors__in=[vendor.id], is_ordered=True).order_by('created_at')
-    recent_orders = orders[:10]
+    # orders = Order.objects.filter(vendors__in=[vendor.id], is_ordered=True).order_by('created_at')
+    # recent_orders = orders[:10]
 
-    # current month's revenue
-    current_month = datetime.datetime.now().month
-    current_month_orders = orders.filter(vendors__in=[vendor.id], created_at__month=current_month)
-    current_month_revenue = 0
-    for i in current_month_orders:
-        current_month_revenue += i.get_total_by_vendor()['grand_total']
+    # # current month's revenue
+    # current_month = datetime.datetime.now().month
+    # current_month_orders = orders.filter(vendors__in=[vendor.id], created_at__month=current_month)
+    # current_month_revenue = 0
+    # for i in current_month_orders:
+    #     current_month_revenue += i.get_total_by_vendor()['grand_total']
     
 
-    # total revenue
-    total_revenue = 0
-    for i in orders:
-        total_revenue += i.get_total_by_vendor()['grand_total']
+    # # total revenue
+    # total_revenue = 0
+    # for i in orders:
+    #     total_revenue += i.get_total_by_vendor()['grand_total']
     context = {
-        'orders': orders,
-        'orders_count': orders.count(),
-        'recent_orders': recent_orders,
-        'total_revenue': total_revenue,
-        'current_month_revenue': current_month_revenue,
+        'vendor':vendor,
+        # 'orders': orders,
+        # 'orders_count': orders.count(),
+        # 'recent_orders': recent_orders,
+        # 'total_revenue': total_revenue,
+        # 'current_month_revenue': current_month_revenue,
     }
     return render(request, 'accounts/vendorDashboard.html', context)
 
